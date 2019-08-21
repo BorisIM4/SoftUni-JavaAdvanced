3f2b82898ae5037d14fbfddc2c74d4b0a0697c65
JRB <jrb@abv.bg>
2019-08-21 12:10:30 +0300

    Test

---
+++ src/L1_Stacks_and_Queues/P1_Test.java
@@ -0,0 +1,11 @@
+package L1_Stacks_and_Queues;
+
+import java.util.Scanner;
+
+public class P1_Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello World!");
+    }
+}
--- src/L3_Sets_and_Maps_Advanced/P1_Parking_Lot.java
+++ src/L3_Sets_and_Maps_Advanced/P1_Parking_Lot.java
@@ -1,10 +1,41 @@
 package L3_Sets_and_Maps_Advanced;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class P1_Parking_Lot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        HashSet<String> carsRegistrationSet = new HashSet<>();
+
+        String[] input = scanner.nextLine().split(", ");
+
+        while (!input[0].equals("END")){
+            String direction = input[0];
+            String carRegistration = input[1];
+
+            if (direction.equals("IN")){
+                carsRegistrationSet.add(carRegistration);
+            }
+            if (direction.equals("OUT")){
+                if (carsRegistrationSet.contains(carRegistration)){
+                    carsRegistrationSet.remove(carRegistration);
+                }
+            }
+
+            input = scanner.nextLine().split(", ");
+
+        }
+
+        if (carsRegistrationSet.isEmpty()){
+            System.out.println("Parking Lot is Empty");
+        } else {
+            for (String element : carsRegistrationSet) {
+                System.out.println(element);
+            }
+        }
+
 
 
     }
