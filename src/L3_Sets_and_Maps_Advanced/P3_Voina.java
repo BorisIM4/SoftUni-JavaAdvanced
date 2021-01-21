382762100ace0a388b77360ccabd0e6e20715135
JRB <jrb@abv.bg>
2021-01-21 18:54:34 +0200

    Lec3-P3

---
+++ src/L3_Sets_and_Maps_Advanced/P3_Voina.java
@@ -0,0 +1,55 @@
+package L3_Sets_and_Maps_Advanced;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P3_Voina {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        LinkedHashSet<Integer> firstDeck = Arrays
+                .stream(scanner
+                        .nextLine()
+                        .split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        LinkedHashSet<Integer> secondDeck = Arrays
+                .stream(scanner
+                        .nextLine()
+                        .split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        int rounds = 50;
+
+        while (rounds-- > 0 && !firstDeck.isEmpty() && !secondDeck.isEmpty()) {
+            int first =  firstDeck.iterator().next();
+
+            int second = secondDeck.iterator().next();
+
+            firstDeck.remove(first);
+            secondDeck.remove(second);
+
+            if (first > second) {
+                firstDeck.add(first);
+                firstDeck.add(second);
+            } else if (second > first) {
+                secondDeck.add(first);
+                secondDeck.add(second);
+            }
+
+
+        }
+
+        if (firstDeck.size() == secondDeck.size()) {
+            System.out.println("Draw!");
+        } else if (firstDeck.size() < secondDeck.size()) {
+            System.out.println("Second player win!");
+        } else if (secondDeck.size() < firstDeck.size()) {
+            System.out.println("First player win!");
+        }
+    }
+}
