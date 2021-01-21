c9feb3076b6cc19219140bcdb19755c7f23f06a0
JRB <jrb@abv.bg>
2021-01-21 18:55:59 +0200

    Lec3-P2, P3

--- src/L2_Multidimentional_Arrays_exercise/P2_Matrix_of_Palindromes_2.java
+++ src/L2_Multidimentional_Arrays_exercise/P2_Matrix_of_Palindromes_2.java
@@ -1,10 +1,20 @@
 package L2_Multidimentional_Arrays_exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P2_Matrix_of_Palindromes_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int[] dimensionsOfMatrix = Arrays
+                .stream(scanner
+                        .nextLine()
+                        .split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int numberOfRows = dimensionsOfMatrix[0];
+        int numberOfColums = dimensionsOfMatrix[1];
 
 
 
--- src/L3_Sets_and_Maps_Advanced_exercise/P1_Unique_Usernames.java
+++ src/L3_Sets_and_Maps_Advanced_exercise/P1_Unique_Usernames.java
@@ -1,11 +1,23 @@
 package L3_Sets_and_Maps_Advanced_exercise;
 
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class P1_Unique_Usernames {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int numberOfInputs = Integer.parseInt(scanner.nextLine());
 
+        Set<String> names = new LinkedHashSet<>();
+
+        for (int name = 0; name < numberOfInputs; name++) {
+            String currentName = scanner.nextLine();
+            names.add(currentName);
+        }
+        names.forEach(name -> {
+            System.out.println(name);
+        });
     }
 }
