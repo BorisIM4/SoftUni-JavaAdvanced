33ca99c7cb2b928b29a36749996d753fc5af021b
JRB <jrb@abv.bg>
2021-01-23 21:27:18 +0200

    Lec3-Exer-P13

---
+++ src/L3_Sets_and_Maps_Advanced_exercise/P13_Srypsko_Unleashed.java
@@ -0,0 +1,18 @@
+package L3_Sets_and_Maps_Advanced_exercise;
+
+import java.util.Scanner;
+
+public class P13_Srypsko_Unleashed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 6; i+=2) {
+            int n1 = i;
+            int n2 = i + 1;
+
+            System.out.println(String.format("N1 => %d",n1));
+            System.out.println(String.format("N2 => %d",n2));
+        }
+
+    }
+}
