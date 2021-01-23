d04bb6fbb90994bb7f16898e49388c3c356e5511
JRB <jrb@abv.bg>
2021-01-23 21:27:26 +0200

    Lec3-Exer-P14

---
+++ src/L3_Sets_and_Maps_Advanced_exercise/P14_Dragon_Army.java
@@ -0,0 +1,11 @@
+package L3_Sets_and_Maps_Advanced_exercise;
+
+import java.util.Scanner;
+
+public class P14_Dragon_Army {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
