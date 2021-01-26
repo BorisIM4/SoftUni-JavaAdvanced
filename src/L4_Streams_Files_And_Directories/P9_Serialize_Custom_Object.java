df300c1c37f6feb2112c2794d988528696dbff7d
JRB <jrb@abv.bg>
2021-01-26 17:57:05 +0200

    Lec4-P9-Marto

--- src/L4_Streams_Files_And_Directories/P9_Serialize_Custom_Object.java
+++ src/L4_Streams_Files_And_Directories/P9_Serialize_Custom_Object.java
@@ -12,36 +12,6 @@ public class P9_Serialize_Custom_Object {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-        String fileName = "D:" +
-                "/Programirane" +
-                "/Java" +
-                "/SoftUni" +
-                "/03-SU-Java-Advanced-2021-01" +
-                "/04. Java-Advanced-Files-and-Streams-Lab-Resources" +
-                "/Files-and-Streams";
 
-        List<String> names = new ArrayList<>();
-
-        names.add("Martin");
-        names.add("Pesho");
-        names.add("Gosho");
-        names.add("Bobi");
-
-        ObjectOutputStream outputStream =
-                new ObjectOutputStream(new FileOutputStream("list.txt"));
-
-        outputStream.writeObject(names);
-
-        outputStream.close();
-
-        ObjectInputStream inputStream =
-                new ObjectInputStream(new FileInputStream("list.txt"));
-
-        List<String> readedNames = (List<String>)inputStream.readObject();
-
-        for (String name : readedNames) {
-            System.out.println(name);
-
-        }
     }
 }
