207f32569b6ceb2e95f65c07c9ccf8f80351f024
JRB <jrb@abv.bg>
2021-01-26 17:44:19 +0200

    Lec4-P8

--- src/L4_Streams_Files_And_Directories/P8_Nested_Folders.java
+++ src/L4_Streams_Files_And_Directories/P8_Nested_Folders.java
@@ -1,4 +1,39 @@
 package L4_Streams_Files_And_Directories;
 
+import java.io.File;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class P8_Nested_Folders {
+    public static void main(String[] args) {
+        String fileName = "D:" +
+                "/Programirane" +
+                "/Java" +
+                "/SoftUni" +
+                "/03-SU-Java-Advanced-2021-01" +
+                "/04. Java-Advanced-Files-and-Streams-Lab-Resources" +
+                "/Files-and-Streams";
+
+        File file = new File(fileName);
+
+        Deque<File> directories = new ArrayDeque<>();
+        directories.offer(file);
+
+        int count = 0;
+        while (!directories.isEmpty()) {
+            File currenetFile= directories.poll();
+            File[] nestedFiles = currenetFile.listFiles();
+
+            for (File nestedFile : nestedFiles) {
+                if (nestedFile.isDirectory()) {
+                    directories.offer(nestedFile);
+                }
+
+            }
+            count++;
+            System.out.println(currenetFile.getName());
+        }
+        System.out.println(count + " folders");
+
+    }
 }
