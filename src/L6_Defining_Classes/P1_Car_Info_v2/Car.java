6be55cb9cf4e1ba3638f326fa4aab87c197cbd41
JRB <jrb@abv.bg>
2021-01-29 20:03:44 +0200

    Lec6-P1v2

---
+++ src/L6_Defining_Classes/P1_Car_Info_v2/Car.java
@@ -0,0 +1,5 @@
+package L6_Defining_Classes.P1_Car_Info_v2;
+
+public class Car {
+
+}
---
+++ src/L6_Defining_Classes/P1_Car_Info_v2/Main.java
@@ -0,0 +1,11 @@
+package L6_Defining_Classes.P1_Car_Info_v2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
