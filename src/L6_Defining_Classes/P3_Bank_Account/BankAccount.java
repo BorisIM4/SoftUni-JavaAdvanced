b30f30b4a76f57e82b07d1208993f82c4336bfa9
JRB <jrb@abv.bg>
2021-01-29 13:13:53 +0200

    Lec6-P3

--- src/L6_Defining_Classes/P3_Bank_Account/BankAccount.java
+++ src/L6_Defining_Classes/P3_Bank_Account/BankAccount.java
@@ -1,4 +1,29 @@
 package L6_Defining_Classes.P3_Bank_Account;
 
 public class BankAccount {
+    private final static double DEFALUT_INTEREST = 0.02;
+    private static double interesetRate = DEFALUT_INTEREST;
+    private static int bankAccountCount = 1;
+    private int id;
+    private double balance;
+
+    BankAccount() {
+        this.id = bankAccountCount++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setInterest(double interest) {
+        BankAccount.interesetRate = interest;
+    }
+
+    void deposit (double amount) {
+        this.balance += amount;
+    }
+
+    public double getInterest(int years) {
+        return BankAccount.interesetRate * years * this.balance;
+    }
 }
--- src/L6_Defining_Classes/P3_Bank_Account/Main.java
+++ src/L6_Defining_Classes/P3_Bank_Account/Main.java
@@ -1,4 +1,71 @@
 package L6_Defining_Classes.P3_Bank_Account;
 
+import java.text.DecimalFormat;
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<BankAccount> listOfBankAccounts = new ArrayList<>();
+
+        DecimalFormat dF = new DecimalFormat("###.######");
+
+        String line = "";
+        while (!("End".equals(line = scanner.nextLine()))) {
+            String[] token = line.split("\\s+");
+            String command = token[0];
+
+            if ("Create".equals(command)) {
+                BankAccount bankAccount = new BankAccount();
+                listOfBankAccounts.add(bankAccount);
+
+                System.out.printf("Account ID%d created%n"
+                        ,bankAccount.getId());
+
+            } else if ("Deposit".equals(command)) {
+                int currentAccId = Integer.parseInt(token[1]);
+                double amountOfMoneyToBeDeposit = Double.parseDouble(token[2]);
+
+                if (currentAccId > listOfBankAccounts.size()) {
+                    System.out.println("Account does not exist");
+                } else {
+                    for (int i = 0; i < listOfBankAccounts.size(); i++) {
+                        if (currentAccId - 1 == i) {
+                            BankAccount currentBankAcc = listOfBankAccounts.get(i);
+                            currentBankAcc.deposit(amountOfMoneyToBeDeposit);
+
+                            System.out.printf("Deposited %s to ID%d%n"
+                                    , dF.format(amountOfMoneyToBeDeposit)
+                                    , currentAccId);
+                        }
+                    }
+                }
+            } else if ("SetInterest".equals(command)) {
+                double currentinterest = Double.parseDouble(token[1]);
+
+                BankAccount bankAccount = new BankAccount();
+                bankAccount.setInterest(currentinterest);
+            } else if ("GetInterest".equals(command)) {
+                int currentAccID = Integer.parseInt(token[1]);
+                int numberOfYears = Integer.parseInt(token[2]);
+
+                if (currentAccID > listOfBankAccounts.size()) {
+                    System.out.println("Account does not exist");
+                } else {
+                    for (int i = 0; i < listOfBankAccounts.size(); i++) {
+                        if (currentAccID - 1 == i) {
+                            BankAccount currentBankAcc = listOfBankAccounts.get(i);
+                            double interest = currentBankAcc.getInterest(numberOfYears);
+
+                            System.out.printf("%.2f%n", interest);
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+    }
 }
