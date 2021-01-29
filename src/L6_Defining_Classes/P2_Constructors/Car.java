273c0e7ac527d8c12ed6043dd171890991a984f6
JRB <jrb@abv.bg>
2021-01-29 13:13:41 +0200

    Lec6-P2

--- src/L6_Defining_Classes/P2_Constructors/Car.java
+++ src/L6_Defining_Classes/P2_Constructors/Car.java
@@ -1,4 +1,43 @@
 package L6_Defining_Classes.P2_Constructors;
 
 public class Car {
+    public String brand;
+    public String model;
+    public int horsePower;
+
+    public Car(String brand) {
+        this.brand = brand;
+        this.model = "unknown";
+        this.horsePower = -1;
+    }
+
+    public Car(String brand, String model, int horsePower) {
+        this.brand = brand;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
 }
--- src/L6_Defining_Classes/P2_Constructors/Main.java
+++ src/L6_Defining_Classes/P2_Constructors/Main.java
@@ -1,4 +1,41 @@
 package L6_Defining_Classes.P2_Constructors;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Car> listOfCars = new LinkedList<>();
+
+        int numberOfCars = Integer.parseInt(scanner.nextLine());
+        for (int car = 0; car < numberOfCars; car++) {
+            String[] tokens = scanner.nextLine().split("\\s+");
+            String brand = tokens[0];
+            if (tokens.length == 1) {
+                Car currentCar = new Car(brand);
+                listOfCars.add(currentCar);
+            } else {
+                String model = tokens[1];
+                int horsePower = Integer.parseInt(tokens[2]);
+                Car currnetCar = new Car(brand, model, horsePower);
+                listOfCars.add(currnetCar);
+            }
+
+        }
+
+        for (Car car : listOfCars) {
+            String brand = car.getBrand();
+            String model = car.getModel();
+            int horsePower = car.getHorsePower();
+
+            System.out.printf("The car is: %s %s - %d HP."
+                    , brand
+                    , model
+                    , horsePower);
+            System.out.println();
+        }
+    }
 }
