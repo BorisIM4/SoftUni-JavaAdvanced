edc88ccbf9f86d59cd72982ee6e3f3886a4f72f1
JRB <jrb@abv.bg>
2021-01-29 13:13:22 +0200

    Lec6-P1

--- src/L6_Defining_Classes/P1_Car_Info/Car.java
+++ src/L6_Defining_Classes/P1_Car_Info/Car.java
@@ -1,4 +1,41 @@
 package L6_Defining_Classes.P1_Car_Info;
 
 public class Car {
+    public String brand;
+    public String model;
+    public int horsePower;
+
+    public Car(String brand, String model, int horsePower) {
+        this.brand = brand;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
+    public Car() {
+
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
 }
--- src/L6_Defining_Classes/P1_Car_Info/Main.java
+++ src/L6_Defining_Classes/P1_Car_Info/Main.java
@@ -1,7 +1,43 @@
 package L6_Defining_Classes.P1_Car_Info;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        List<Car> carsList = new LinkedList<>();
+
+        int numberOfcars = Integer.parseInt(scanner.nextLine());
+
+        for (int cars = 0; cars < numberOfcars; cars++) {
+            String[] inputDataCar = scanner.nextLine().split("\\s+");
+
+            String currentCarBrand = inputDataCar[0];
+            String currentCarModel = inputDataCar[1];
+            int currentCarHorsePower = Integer.parseInt(inputDataCar[2]);
+
+            Car car = new Car();
+            car.setBrand(currentCarBrand);
+            car.setModel(currentCarModel);
+            car.setHorsePower(currentCarHorsePower);
+
+            carsList.add(car);
+        }
+
+        for (Car car : carsList) {
+            String brand = car.getBrand();
+            String model = car.getModel();
+            int horsePower = car.getHorsePower();
+
+            System.out.printf("The car is: %s %s - %d HP."
+                    , brand
+                    , model
+                    , horsePower);
+            System.out.println();
+        }
     }
 }
