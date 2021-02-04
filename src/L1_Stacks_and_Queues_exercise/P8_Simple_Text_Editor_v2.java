6b0cfbb1611b6ce5f93c07c9684920036d63efaf
JRB <jrb@abv.bg>
2021-02-04 18:13:27 +0200

    Lec1-Exer-P8v2

---
+++ src/L1_Stacks_and_Queues_exercise/P8_Simple_Text_Editor_v2.java
@@ -0,0 +1,11 @@
+package L1_Stacks_and_Queues_exercise;
+
+import java.util.Scanner;
+
+public class P8_Simple_Text_Editor_v2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
