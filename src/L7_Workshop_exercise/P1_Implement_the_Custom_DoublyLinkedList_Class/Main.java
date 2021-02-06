8444312ed124f04aa69f0c9db37dfec887fb89fc
JRB <jrb@abv.bg>
2021-02-06 23:35:47 +0200

    Lec7-Exer-P1

---
+++ src/L7_Workshop_exercise/P1_Implement_the_Custom_DoublyLinkedList_Class/Main.java
@@ -0,0 +1,11 @@
+package L7_Workshop_exercise.P1_Implement_the_Custom_DoublyLinkedList_Class;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
