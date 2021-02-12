fd9b813dd1c10942fbf9c60166c16fc9a4c06404
JRB <jrb@abv.bg>
2021-02-12 21:27:43 +0200

    Lec1-Exam-P3

---
+++ src/L10_Exam_Preparation_One/P3_Openning/Main.java
@@ -0,0 +1,11 @@
+package L10_Exam_Preparation_One.P3_Openning;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
