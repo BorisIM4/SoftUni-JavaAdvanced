66351d960451f36e223ef34354d7aae271de4e2d
JRB <jrb@abv.bg>
2021-02-19 21:30:23 +0200

    Lec10-Exam4-P1

---
+++ src/L10_Exam_Preparation_Two/Exam_2020_06_28/Main.java
@@ -0,0 +1,11 @@
+package L10_Exam_Preparation_Two.Exam_2020_06_28;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
