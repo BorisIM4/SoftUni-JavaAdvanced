64e2516e8cbe90ec0152a234a7bba67c2b2d184c
JRB <jrb@abv.bg>
2021-02-25 16:11:04 +0200

    Lec12-FinalExam-Theory

---
+++ src/L12_Exam/Theory/Main.java
@@ -0,0 +1,14 @@
+package L12_Exam.Theory;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] matrix = new int[3][3];
+
+        System.out.println();
+
+    }
+}
